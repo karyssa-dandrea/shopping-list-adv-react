528d4d92ee7e91db5113d68cbe03883e440fb50a
karyssa-dandrea <karyssadandrea@gmail.com>
2022-02-14 16:59:37 -0800

    added my switch and case for adding and deleteing items

--- src/views/Shopping/Shopping.js
+++ src/views/Shopping/Shopping.js
@@ -6,6 +6,35 @@ const initialItems = [
   { id: 2, text: 'Potatoes', done: false },
 ];
 
+function itemsReducer(items, action) {
+  switch (action.type) {
+    case 'add': {
+      return [
+        ...items,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+      ];
+    }
+    case 'update': {
+      return items.map((item) => {
+        if (item.id === action.task.id) {
+          return action.task;
+        }
+        return item;
+      });
+    }
+    case 'delete': {
+      return items.filter((item) => item.id !== action.id);
+    }
+    default: {
+      throw Error(`Unknown action: ${action.type}`);
+    }
+  }
+}
+
 export default function Shopping() {
   return <div>Shopping</div>;
 }
