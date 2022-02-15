64e786d4c34ff8e2f521eaaad98439c14bb1d20f
karyssa-dandrea <karyssadandrea@gmail.com>
2022-02-15 14:48:58 -0800

    got my add item to work yesss

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { useState } from 'react';
 
-export default function AddItem() {
-  return <div>AddItem</div>;
+export default function AddItem({ addItem }) {
+  const [newItem, setNewItem] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setNewItem('');
+    addItem(newItem);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        Add To List:
+        <input
+          placeholder="New Item"
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 }
