dfe6bcb622eddd2909d815c78f23f3f3c51274c7
karyssa-dandrea <karyssadandrea@gmail.com>
2022-02-14 16:22:40 -0800

    created my files and initial items

---
+++ src/views/Shopping/Shopping.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const initialItems = [
+  { id: 0, text: 'Oatmilk', done: false },
+  { id: 1, text: 'Jalapenos', done: false },
+  { id: 2, text: 'Potatoes', done: false },
+];
+
+export default function Shopping() {
+  return <div>Shopping</div>;
+}
