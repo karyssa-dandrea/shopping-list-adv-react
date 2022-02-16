2cf3fd0e45ac7f55b8d4f1a5526491587237d70e
karyssa-dandrea <karyssadandrea@gmail.com>
2022-02-15 17:49:52 -0800

    got two tests to pass

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
+import Shopping from './views/Shopping/Shopping';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('test for behavioral and components', () => {
+  render(<Shopping />);
+
+  const itemInput = screen.getByRole('textbox');
+  expect(itemInput).toBeInTheDocument();
+
+  userEvent.type(itemInput, 'Oatmilk');
+  expect(itemInput.value).toBe('Oatmilk');
+
+  const addButton = screen.getByRole('button', { name: 'Add' });
+  expect(addButton).toBeInTheDocument();
+  userEvent.click(addButton);
 });
