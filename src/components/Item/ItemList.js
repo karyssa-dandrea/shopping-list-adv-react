f09acefc283761f33839afb20b9d3f8e59a9f451
karyssa-dandrea <karyssadandrea@gmail.com>
2022-02-15 16:25:18 -0800

    got it to work right

--- src/components/Item/ItemList.js
+++ src/components/Item/ItemList.js
@@ -7,8 +7,6 @@ export default function ItemList({ items, deleteItem, updateItem }) {
       <ul>
         {items.map((item) => (
           <li key={item.id} className="list">
-            <p>{item.text}</p>
-            <input type="checkbox" />
             <Item deleteItem={deleteItem} updateItem={updateItem} item={item} />
           </li>
         ))}
