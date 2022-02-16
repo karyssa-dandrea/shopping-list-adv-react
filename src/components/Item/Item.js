c1494cb7782a04303800c109586b9bed8e9cb308
karyssa-dandrea <karyssadandrea@gmail.com>
2022-02-15 16:20:55 -0800

    got my edit to work but it looks weird

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,5 +1,52 @@
 import React from 'react';
+import { useState } from 'react';
 
-export default function Item() {
-  return <div>Item</div>;
+export default function Item({ item, updateItem, deleteItem }) {
+  const [editing, setEditing] = useState(false);
+  let itemContent;
+  if (editing) {
+    itemContent = (
+      <div>
+        <input
+          value={item.text}
+          onChange={(e) => {
+            updateItem({
+              ...item,
+              text: e.target.value,
+            });
+          }}
+        />
+        <button type="button" onClick={() => setEditing(false)}>
+          Save Changes
+        </button>
+      </div>
+    );
+  } else {
+    itemContent = (
+      <div>
+        <p>{item.text}</p>
+        <button type="button" onClick={() => setEditing(true)}>
+          Edit
+        </button>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <input
+        type="checkbox"
+        checked={item.done}
+        onChange={(e) => {
+          updateItem({
+            ...item,
+            done: e.target.checked,
+          });
+        }}
+      />
+      {itemContent}
+      <button type="button" onClick={() => deleteItem(item.id)}>
+        Delete
+      </button>
+    </div>
+  );
 }
--- src/components/Item/ItemList.js
+++ src/components/Item/ItemList.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import Item from './Item';
 
-export default function ItemList({ items }) {
+export default function ItemList({ items, deleteItem, updateItem }) {
   return (
     <div>
       <ul>
@@ -8,6 +9,7 @@ export default function ItemList({ items }) {
           <li key={item.id} className="list">
             <p>{item.text}</p>
             <input type="checkbox" />
+            <Item deleteItem={deleteItem} updateItem={updateItem} item={item} />
           </li>
         ))}
       </ul>
