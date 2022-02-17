e08a960db411e182069f87b9c89c3debf8c10fea
karyssa-dandrea <karyssadandrea@gmail.com>
2022-02-17 15:44:39 -0800

    fixed my list context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Shopping from './views/Shopping/Shopping';
 import Header from './components/Header/Header';
+import ListProvider from './context/ListContext';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Shopping />
-    </div>
+    <ListProvider>
+      <div className="App">
+        <Header />
+        <Shopping />
+      </div>
+    </ListProvider>
   );
 }
 
--- src/context/ListContext.js
+++ src/context/ListContext.js
@@ -5,36 +5,36 @@ const initialItems = [
   { id: 0, text: 'Oatmilk', done: false },
   { id: 1, text: 'Jalapenos', done: false },
   { id: 2, text: 'Potatoes', done: false },
-
-  function itemsReducer(items, action) {
-    switch (action.type) {
-      case 'add': {
-        return [
-          ...items,
-          {
-            id: action.id,
-            text: action.text,
-            done: false,
-          },
-        ];
-      }
-      case 'update': {
-        return items.map((item) => {
-          if (item.id === action.task.id) {
-            return action.task;
-          }
-          return item;
-        });
-      }
-      case 'delete': {
-        return items.filter((item) => item.id !== action.id);
-      }
-      default: {
-        throw Error(`Unknown action: ${action.type}`);
-      }
-    }
-  },
 ];
+
+function itemsReducer(items, action) {
+  switch (action.type) {
+    case 'add': {
+      return [
+        ...items,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+      ];
+    }
+    case 'update': {
+      return items.map((item) => {
+        if (item.id === action.task.id) {
+          return action.task;
+        }
+        return item;
+      });
+    }
+    case 'delete': {
+      return items.filter((item) => item.id !== action.id);
+    }
+    default: {
+      throw Error(`Unknown action: ${action.type}`);
+    }
+  }
+}
 
 export const ListContext = createContext();
 
