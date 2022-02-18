5839a97c7830ee2e2ab7b4c5e8abcaa335ce5170
karyssa-dandrea <karyssadandrea@gmail.com>
2022-02-17 16:53:03 -0800

    got my header to show list of items

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { useList } from '../../context/ListContext';
 
 export default function Header() {
-  return <div>Header</div>;
+  const { items } = useList();
+
+  return (
+    <header>
+      <h2>Shopping List Items:</h2>
+      <span>{items.length} on your list</span>
+    </header>
+  );
 }
